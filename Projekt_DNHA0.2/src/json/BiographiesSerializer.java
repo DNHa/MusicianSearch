2237294c82bfeb0d4375c388bd1177a1533e2a24
Student <Student@10.101.12.126>
2015-12-01 14:53:32 +0100

    aktuell

--- Projekt_DNHA0.2/src/json/BiographiesSerializer.java
+++ Projekt_DNHA0.2/src/json/BiographiesSerializer.java
@@ -1,33 +1,33 @@
-//package json;
-//
-//import java.lang.reflect.Type;
-//
-//import com.google.gson.JsonArray;
-//import com.google.gson.JsonDeserializationContext;
-//import com.google.gson.JsonDeserializer;
-//import com.google.gson.JsonElement;
-//import com.google.gson.JsonParseException;
-//
-//public class BiographiesSerializer implements JsonDeserializer<Biographies>{
-//
-//	@Override
-//	public Biographies deserialize(JsonElement jsonElement, Type arg1, JsonDeserializationContext arg2)
-//			throws JsonParseException {
-//			Biographies biographies = new Biographies();
-//			
-//			JsonElement responseElement = jsonElement.getAsJsonObject().get("response");		
-//			JsonElement biographiesElement = responseElement.getAsJsonObject().get("biographies");
-//			JsonArray biographiesArray = biographiesElement.getAsJsonArray();
-//			
-//			for (JsonElement biography : biographiesArray) {
-//				JsonElement bio = biography.getAsJsonObject().get("text");
-//				if(bio != null){
-//					biographies.addBioTxt(bio.getAsString());
-//				}
-//			
-//			}
-//			
-//			return biographies;
-//	}
-//
-//}
+package json;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+public class BiographiesSerializer implements JsonDeserializer<Biographies>{
+
+	@Override
+	public Biographies deserialize(JsonElement jsonElement, Type arg1, JsonDeserializationContext arg2)
+			throws JsonParseException {
+			Biographies biographies = new Biographies();
+			
+			JsonElement responseElement = jsonElement.getAsJsonObject().get("response");		
+			JsonElement biographiesElement = responseElement.getAsJsonObject().get("biographies");
+			JsonArray biographiesArray = biographiesElement.getAsJsonArray();
+			
+			for (JsonElement biography : biographiesArray) {
+				JsonElement bio = biography.getAsJsonObject().get("text");
+				if(bio != null){
+					biographies.addBioTxt(bio.getAsString());
+				}
+			
+			}
+			
+			return biographies;
+	}
+
+}
